90a8f61e81e2ed71e7f1cdd56c5550b8c89785b6
Oliver Martell <oliver.martell@gmail.com>
2014-03-16 13:24:29 +0000

    Implement oopsie monad exercise

--- src/fp_oo/oopsie-monad.clj
+++ src/fp_oo/oopsie-monad.clj
@@ -1,27 +1,52 @@
-(ns fp-oo.oopsie [:use [clojure.algo.monads]])
+(ns fp-oo.oopsie
+  [:use [clojure.algo.monads]])
 
 (def decider
-     (fn [value continuation]
-       (if (nil? value)
-         nil
-         (continuation value))))
+  (fn [value continuation]
+    (if (nil? value)
+      nil
+      (continuation value))))
 
 (def maybe-monad
-     (monad [m-result identity
-             m-bind   decider]))
+  (monad [m-result identity
+          m-bind   decider]))
 
-(with-monad maybe-monad
-  (domonad [a nil
-            b (+ 1 a)] ; would blow up
+(with-monad error-monad
+  (domonad [a 2
+            b (+ 1 a)]
            b))
 
-;; Error utilities
 (def oops!
-     (fn [reason & args]
-       (with-meta (merge {:reason reason}
-                         (apply hash-map args))
-                  {:type :error})))
+  (fn [reason & args]
+    (with-meta (merge {:reason reason}
+                      (apply hash-map args))
+      {:type :error})))
 
 (def oopsie?
-     (fn [value]
-       (= (type value) :error)))
+  (fn [value]
+    (= (type value) :error)))
+
+(defn error-decider
+  [value continuation]
+  (if (oopsie? value)
+       value
+       (continuation value)))
+
+(def error-monad
+  (monad [m-result identity
+          m-bind error-decider]))
+
+(def factorial
+  (fn [n]
+    (cond (< n 0)
+          (oops! "Factorial can never be less than zero." :number n)
+          (< n 2)
+          1
+          :else
+          (* n (factorial (dec n))))))
+
+(with-monad error-monad
+    (domonad [big-number (factorial -1)
+              even-bigger (* 2 big-number)]
+             (repeat even-bigger :a)))
+
